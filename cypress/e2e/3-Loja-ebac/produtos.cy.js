12e78227edc554c74129a91758b9a9e274c6f6ca
Ronan Soares <ronan.soares.work@gmail.com>
2024-08-19 18:21:49 -0300

    Adicionado produtoss

---
+++ cypress/e2e/3-Loja-ebac/produtos.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress"/>
+
+
+describe('Funcionalidade: Produtos', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/#')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get(' .block-inner')
+            //.first()
+            //.last()
+            .eq(5)
+            .click()
+
+        
+     
+
+        
+        
+
+    });
+    
+});
