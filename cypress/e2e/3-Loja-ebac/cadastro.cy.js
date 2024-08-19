75b2fc7f34f9b3f67f431b3835388dcb7a631176
Ronan Soares <ronan.soares.work@gmail.com>
2024-08-19 17:53:16 -0300

    Adicionado cadastro com variaveis e faker-js

---
+++ cypress/e2e/3-Loja-ebac/cadastro.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress"/>
+
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade: Cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/my-account/')
+
+    });
+
+    it('Deve completar o cadastro', () => {
+        var nome = faker.person.firstName()
+        var email = faker.internet.email(nome)
+        var senha = faker.internet.password()
+        var sobrenome = faker.person.lastName()
+
+        cy.get('#reg_email').type(email)
+        cy.get('#reg_password').type(senha)
+        cy.get(':nth-child(4) > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nome)
+        cy.get('#account_last_name').type(sobrenome)
+        cy.wait(5000)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('exist')
+
+
+    });
+});
